83ed75591d4e1d297545dd7899b9bd09df900d53
Štefan Varga <128626278+svarga00@users.noreply.github.com>
2026-06-03 19:02:11 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,6 +19,7 @@ export default defineConfig({
 
   integrations: [
     sitemap({
+      filter: (page) => !page.includes('/web-brief'),
       i18n: {
         defaultLocale: 'sk',
         locales: {
